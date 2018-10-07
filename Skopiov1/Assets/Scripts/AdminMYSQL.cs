c571ac7948babe7500cb0116e66005cd0e0f4644
BenackFL <benjamin.fregoso@alumnos.udg.mx>
2018-10-07 11:01:14 -0500

    Cambios para base

---
+++ Skopiov1/Assets/Scripts/AdminMYSQL.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data.MySqlClient;
+
+public class AdminMYSQL : MonoBehaviour {
+
+    public string servidorBaseDatos;
+    public string nombreBaseDatos;
+    public string usuarioBaseDatos;
+    public string contrasenaBaseDatos;
+
+    private string datosConexion;
+    private MySqlConnection conexion;
+	// Use this for initialization
+	void Start () {
+
+        datosConexion = "Server=" + servidorBaseDatos + ";Database="+nombreBaseDatos+";Uid=" + usuarioBaseDatos+";Pwd="+contrasenaBaseDatos+";";
+
+        ConectarConServidor();
+	}
+	
+	private void ConectarConServidor()
+    {
+        conexion = new MySqlConnection(datosConexion);
+
+        try
+        {
+            conexion.Open();
+            Debug.Log("Conexion con DB correcta");
+        }
+        catch(MySqlException e)
+        {
+            Debug.LogError("Error: "+e);
+        }
+    }
+
+    public MySqlDataReader Select(string _select)
+    {
+        MySqlCommand cmd = conexion.CreateCommand();
+        cmd.CommandText = "SELECT * FROM " + _select;
+        MySqlDataReader Resultado = cmd.ExecuteReader();
+        return Resultado;
+        
+    }
+}
---
+++ Skopiov1/Assets/Scripts/login.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MySql.Data.MySqlClient;
+
+public class login : MonoBehaviour {
+
+    public InputField usuariotxt;
+    public InputField contrasenatxt;
+     
+
+    public void logear()
+    {
+        string _log = "`usuarios` WHERE `nombre_usuario` LIKE '"+usuariotxt.text + "' AND `pass_usuario` LIKE '" + contrasenatxt.text+"'";
+
+        AdminMYSQL _adminMYSQL = GameObject.Find("AdministradorBaseDatos").GetComponent<AdminMYSQL>();
+        MySqlDataReader Resultado = _adminMYSQL.Select(_log);
+
+        if (Resultado.HasRows)
+        {
+            Debug.Log("Login correcto");
+        }
+        else
+        {
+            Debug.Log("Login incorrecto");
+        }
+        Resultado.Close();
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
